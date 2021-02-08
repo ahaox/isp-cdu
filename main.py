a92f16f141f3933c224773e34680e60af9216d8f
xuhao <73208206+ahaox@users.noreply.github.com>
2021-02-08 22:09:15 +0800

    author:ahao

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Author: ahao
+# Time: 2021/2/7 22:42
+
+from spider import IspService
+
+if __name__ == "__main__":
+    ispService = IspService()
+    ispService.startClock()
